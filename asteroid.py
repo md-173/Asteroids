0f51925b409ccd6d136c9b6a7301d1fc79187f69
md-173 <durkanmick@gmail.com>
2026-08-08 12:15:34 +0800

    Add player movement

---
+++ asteroid.py
@@ -0,0 +1,5 @@
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame  # pyright: ignore[reportMissingImports]
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,7 +11,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -16,7 +30,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
